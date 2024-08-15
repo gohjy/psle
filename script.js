847570627aad5880ee657e0e9b97b71042ab24f5
gohjy <154421097+gohjy@users.noreply.github.com>
2024-08-15 15:35:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let h1 = document.querySelector("h1");
 
 let ending = new Date(2024, 9, 2, 10, 50);
+const f = (x, y) => Math.floor(x;
 
 function update() {
   let now = new Date();
   let left = ending - now;
-  let days = left // (24 * 60 * 60 * 1000);
-  let hrs = (left // (60 * 60 * 1000)) - (24 * days);
-  let mins = (left // (60 * 1000)) - (24 * 60 * days) - (60 * hrs);
-  let secs = (left // 1000) - (24 * 60 * 60 * days) - (60 * 60 * hrs) - (60 * mins);
+  let days = f(left / (24 * 60 * 60 * 1000);
+  let hrs = f(left, (60 * 60 * 1000)) - (24 * days);
+  let mins = f(left, (60 * 1000)) - (24 * 60 * days) - (60 * hrs);
+  let secs = f(left, 1000) - (24 * 60 * 60 * days) - (60 * 60 * hrs) - (60 * mins);
 
   h1.innerHTML = `${days} <small>days</small> ${hrs}:${mins}:${secs}`;
 }
