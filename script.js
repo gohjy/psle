4331a57e284cb6deb18c2c68fc09cd74eafa1c96
gohjy <154421097+gohjy@users.noreply.github.com>
2024-08-17 05:07:15 +0000

    add support for when psle is over

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let h1 = document.querySelector("#h1area");
+let h2 = document.querySelector("#h2area");
+let h3 = document.querySelector("#h3area");
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 let ending = new Date(2024, 9, 2, 10, 50);
 const f = (x, y) => Math.floor(x/y);
@@ -6,6 +10,13 @@ const f = (x, y) => Math.floor(x/y);
 function update() {
   let now = new Date();
   let left = ending - now;
+  if (left <= 0) {
+    let formattedDate = `${now.getDate()} ${months[now.getMonth()]} ${now.getFullYear()} ${now.getHours().toString().padStart(2, "0")}:${now.getMinutes().toString().padStart(2, "0")}:${now.getSeconds().toString().padStart(2, "0")}`;
+    h2.textContent = formattedDate;
+    h1.innerHTML = "PSLE has ended!";
+    h3.style = "display: none;";
+    return;
+  }
   let days = f(left, (24 * 60 * 60 * 1000));
   let hrs = f(left, (60 * 60 * 1000)) - (24 * days);
   let mins = f(left, (60 * 1000)) - (24 * 60 * days) - (60 * hrs);
